e0f82b5381b2738570a7fbc3bd431327cd0691d0
MatiasLoiseau <matiasloiseau@gmail.com>
2017-11-22 12:44:52 -0300

    re-create, re-name, same hello world

--- rest_framework/views.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-def home(request):
-	return HttpResponse("Hello World !")
