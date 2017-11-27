60480d65f272ff5f6fc2c5f8c18af6f752875179
MatiasLoiseau <matiasloiseau@gmail.com>
2017-11-26 21:20:34 -0300

    Agregado test de post, put y delete

--- cuenta/tests.py
+++ cuenta/tests.py
@@ -1,23 +1,40 @@
+import json
 from django.test import TestCase
 from models import Cuenta
 #from rest_framework.test import APIRequestFactory
+from rest_framework.test import APITestCase
+from rest_framework.test import APIClient
 
 # Create your tests here.
 #factory = APIRequestFactory()
 #request = factory.post('/controlgastos/cuentas/', {'nombre': 'nombrecualquiera'})
 
 
+class CuentaTestCase(APITestCase):
+    # Test module for Cuenta model 
 
+    def test_post_cuenta(self):
+        cuenta_data = {
+            "nombre": "testnombre",
+        }
+        response = self.client.post('/controlgastos/cuentas/', cuenta_data, format='json')
+        self.assertEqual(201, response.status_code)
 
-class CuentaTest(TestCase):
-    """ Test module for Cuenta model """
-
-    def setUp(self):
-        Cuenta.objects.create(nombre='CuentaLoca')
-        Cuenta.objects.create(nombre='OtraCuentaLoca')
-
-    def test_cuenta_nombre(self):
-        cuenta_uno = Cuenta.objects.get(nombre='CuentaLoca')
-        cuenta_dos = Cuenta.objects.get(nombre='OtraCuentaLoca')
-        self.assertEqual(cuenta_uno.get_name(), "CuentaLoca")
-        self.assertEqual(cuenta_dos.get_name(), "OtraCuentaLoca")
+    def test_delete_cuenta(self):
+        cuenta_data = {
+            "nombre": "testnombrecualquiera",
+        }
+        response2 = self.client.post('/controlgastos/cuentas/', cuenta_data, format='json')
+        response3 = self.client.delete('/controlgastos/cuentas/1/', format='json')
+        self.assertEqual(410, response3.status_code)
+        
+    def test_put_cuenta(self):
+        cuenta_data = {
+            "nombre": "testnombrecualquiera",
+        }
+        cuenta_data_modificada = {
+            "nombre": "testnombrecualquiera",
+        }
+        response2 = self.client.post('/controlgastos/cuentas/', cuenta_data, format='json')
+        response3 = self.client.put('/controlgastos/cuentas/1/', cuenta_data_modificada, format='json')
+        self.assertEqual(200, response3.status_code)
