270a980571f5e628694c86a676b3ec6db0d7cc97
vscaparelli <victoria.caparelli@gmail.com>
2017-12-09 14:35:30 -0300

    Add movimiento fixture

--- usuario/urls.py
+++ usuario/urls.py
@@ -3,6 +3,6 @@ from usuario import views
 
 urlpatterns = [
     url(r'^$', views.usuario_list),
-    url(r'^cuenta/(?P<pk>[0-9]+)/$', views.usuario_get),
+    url(r'^(?P<pk>[0-9]+)/cuenta/$', views.usuario_get),
     url(r'^(?P<pk>[0-9]+)/$', views.usuario_modify)
 ]
