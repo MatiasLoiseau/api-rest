509c9874005a6e3959baeb9dcec2cdf037731950
MatiasLoiseau <matiasloiseau@gmail.com>
2017-12-13 00:34:01 -0300

    Update test cuenta y movimiento

--- cuenta/tests.py
+++ cuenta/tests.py
@@ -1,27 +1,64 @@
 from rest_framework.test import APITestCase
 from django.core.management import call_command
+import json
 
-
+#TEST DEL RECURSO CUENTA
 class CuentaTestCase(APITestCase):
     # Test module for Cuenta model
 
+    '''Alta de usuario'''
+
+    #Testeo creación de una cuenta
     def test_post_cuenta(self):
         cuenta_data = {u'nombre': u'testnombre'}
         response = self.client.post('/controlgastos/cuentas/', cuenta_data, format='json')
         self.assertEqual(201, response.status_code)
 
+    #Testeo que no se permita la creación de la cuenta si no se pasan los parametros considerados como obligatorios
+    def test_bad_request_field_required_cuenta(self):
+        cuenta_data_wrong = {}
+        response_post_wrong = self.client.post('/controlgastos/cuentas/', cuenta_data_wrong, format='json')
+        self.assertEqual(400, response_post_wrong.status_code)
+
+    '''Consulta de cuentas'''
+
+    #Testeo la consulta de las cuentas
+    def test_get_cuenta(self):
+        call_command('loaddata', 'data_acc.json', app_label='cuenta') #Cargo las cuentas ya creadas
+        response = self.client.get('/controlgastos/cuentas/')
+        self.assertEqual(200, response.status_code)
+        
+    #Testeo la consulta de una cuenta en especifica
+    def test_get_cuenta_bypk(self):
+        call_command('loaddata', 'data_acc.json', app_label='cuenta') #Cargo las cuentas ya creadas
+        response = self.client.get('/controlgastos/cuentas/1/')
+        self.assertEqual(200, response.status_code)
+
+    '''Eliminacion de una cuenta'''
+
+    #Testeo borrado de una cuenta
     def test_delete_cuenta(self):
         call_command('loaddata', 'data_acc.json', app_label='cuenta')
         responseDelete = self.client.delete('/controlgastos/cuentas/1/', format='json')
         self.assertEqual(200, responseDelete.status_code)
 
+    #Testeo intento de borrado de una cuenta inexistente
+    def test_not_found_delete_cuenta_erronea(self):
+        response_delete = self.client.delete('/controlgastos/cuentas/99/', format='json')
+        self.assertEqual(404, response_delete.status_code)
+
+    '''Modificacion de una cuenta'''
+
+    #Testeo modificación de una cuenta
     def test_put_cuenta(self):
         call_command('loaddata', 'data_acc.json', app_label='cuenta')
         cuenta_data_modificada = {u'nombre': u'testnombremodificado'}
         responsePut = self.client.put('/controlgastos/cuentas/1/', cuenta_data_modificada, format='json')
         self.assertEqual(200, responsePut.status_code)
 
-    def test_bad_request_field_required_cuenta(self):
-        cuenta_data_wrong = {}
-        response_post_wrong = self.client.post('/controlgastos/cuentas/', cuenta_data_wrong, format='json')
-        self.assertEqual(400, response_post_wrong.status_code)
+    #Testeo modificación de una cuenta con envio incorrecto de parametros
+    def test_bad_request_put_usuario(self):
+        call_command('loaddata', 'data_acc.json', app_label='cuenta') #Cargo las cuentas ya creadas
+        cuenta_data_modificada = {}
+        response_put = self.client.put('/controlgastos/cuentas/1/', cuenta_data_modificada, format='json')
+        self.assertEqual(400, response_put.status_code)
--- movimiento/tests.py
+++ movimiento/tests.py
@@ -1,62 +1,113 @@
 from rest_framework.test import APITestCase
 from django.core.management import call_command
+import json
 
+#TEST DEL RECURSO Movimiento
 class MovimientoTestCase(APITestCase):
-    # Test module for Movimiento model
+    
+    '''Alta de movimiento'''
+
+    #Testeo creación de un movimiento
     def test_post_movimiento(self):
-        call_command('loaddata', 'data_acc.json', app_label='movimiento')
-        call_command('loaddata', 'data_usr.json', app_label='movimiento')
-        call_command('loaddata', 'data_cate.json', app_label='movimiento')
+        call_command('loaddata', 'data_acc.json', app_label='movimiento') #Cargo las cuentas ya creadas
+        call_command('loaddata', 'data_usr.json', app_label='movimiento') #Cargo los usuarios ya creados
+        call_command('loaddata', 'data_cate.json', app_label='movimiento') #Cargo las categorias ya creadas
         movimiento_data = {u'monto': u'20000', u'categoria': u'1', u'user': u'1'}
         response_post = self.client.post('/controlgastos/movimientos/', movimiento_data, format='json')
         self.assertEqual(201, response_post.status_code)
 
-
-    def test_delete_movimiento(self):
-        call_command('loaddata', 'data_acc.json', app_label='movimiento')
-        call_command('loaddata', 'data_usr.json', app_label='movimiento')
-        call_command('loaddata', 'data_cate.json', app_label='movimiento')
-        call_command('loaddata', 'data_mov.json', app_label='movimiento')
-        response_delete = self.client.delete('/controlgastos/movimientos/1/', format='json')
-        self.assertEqual(200, response_delete.status_code)
-
-    def test_put_movimiento(self):
-        call_command('loaddata', 'data_acc.json', app_label='movimiento')
-        call_command('loaddata', 'data_usr.json', app_label='movimiento')
-        call_command('loaddata', 'data_cate.json', app_label='movimiento')
-        call_command('loaddata', 'data_mov.json', app_label='movimiento')
-        movimiento_data_modificada = {u'monto': u'20000', u'categoria': u'1', u'user': u'1',u'descripcion':u'alquileres'}
-        response_put = self.client.put('/controlgastos/movimientos/1/', movimiento_data_modificada, format='json')
-        self.assertEqual(200, response_put.status_code)
-
+    #Testeo que indique Bad Request en caso de que se quiera crear un movimiento pasando parametros de tipo incorrecto
     def test_bad_request_incorrect_type_movimiento(self):
-        call_command('loaddata', 'data_acc.json', app_label='movimiento')
-        call_command('loaddata', 'data_usr.json', app_label='movimiento')
-        call_command('loaddata', 'data_cate.json', app_label='movimiento')
+        call_command('loaddata', 'data_acc.json', app_label='movimiento') #Cargo las cuentas ya creadas
+        call_command('loaddata', 'data_usr.json', app_label='movimiento') #Cargo los usuarios ya creados
+        call_command('loaddata', 'data_cate.json', app_label='movimiento') #Cargo las categorias ya creadas
         movimiento_data_wrong = {u'monto': u'20000', u'categoria': u'SoyUnError', u'user': u'1'}
         response_post_wrong = self.client.post('/controlgastos/movimientos/', movimiento_data_wrong, format='json')
         self.assertEqual(400, response_post_wrong.status_code)
 
+    #Testeo que no se permita la creación del movimiento si no se pasan los parametros considerados como obligatorios
     def test_bad_request_field_required_movimiento(self):
-        call_command('loaddata', 'data_acc.json', app_label='movimiento')
-        call_command('loaddata', 'data_usr.json', app_label='movimiento')
-        call_command('loaddata', 'data_cate.json', app_label='movimiento')
+        call_command('loaddata', 'data_acc.json', app_label='movimiento') #Cargo las cuentas ya creadas
+        call_command('loaddata', 'data_usr.json', app_label='movimiento') #Cargo los usuarios ya creados
+        call_command('loaddata', 'data_cate.json', app_label='movimiento') #Cargo las categorias ya creadas
         movimiento_data_wrong = {u'monto': u'20000', u'categoria': u'1'}
         response_post_wrong = self.client.post('/controlgastos/movimientos/', movimiento_data_wrong, format='json')
         self.assertEqual(400, response_post_wrong.status_code)
 
+    #Testeo la creacion de un movimiento asociado a una categoria inexistente
     def test_bad_request_no_encuentra_recurso_movimiento(self):
-        call_command('loaddata', 'data_acc.json', app_label='movimiento')
-        call_command('loaddata', 'data_usr.json', app_label='movimiento')
-        call_command('loaddata', 'data_cate.json', app_label='movimiento')
+        call_command('loaddata', 'data_acc.json', app_label='movimiento') #Cargo las cuentas ya creadas
+        call_command('loaddata', 'data_usr.json', app_label='movimiento') #Cargo los usuarios ya creados
+        call_command('loaddata', 'data_cate.json', app_label='movimiento') #Cargo las categorias ya creadas
         movimiento_data_wrong = {u'monto': u'20000', u'categoria': u'999', u'user': u'1'}
         response_post_wrong = self.client.post('/controlgastos/movimientos/', movimiento_data_wrong, format='json')
         self.assertEqual(400, response_post_wrong.status_code)
 
+    #Testeo la creacion de un movimiento asociado a un usuario inexistente
     def test_bad_request_no_encuentra_recurso_2_movimiento(self):
-        call_command('loaddata', 'data_acc.json', app_label='movimiento')
-        call_command('loaddata', 'data_usr.json', app_label='movimiento')
-        call_command('loaddata', 'data_cate.json', app_label='movimiento')
+        call_command('loaddata', 'data_acc.json', app_label='movimiento') #Cargo las cuentas ya creadas
+        call_command('loaddata', 'data_usr.json', app_label='movimiento') #Cargo los usuarios ya creados
+        call_command('loaddata', 'data_cate.json', app_label='movimiento') #Cargo las categorias ya creadas
         movimiento_data_wrong = {u'monto': u'20000', u'categoria': u'1', u'user': u'999'}
         response_post_wrong = self.client.post('/controlgastos/movimientos/', movimiento_data_wrong, format='json')
         self.assertEqual(400, response_post_wrong.status_code)
+
+
+    '''Consulta de movimiento'''
+
+    #Testeo la consulta de los movimientos
+    def test_get_movimiento(self):
+        call_command('loaddata', 'data_acc.json', app_label='movimiento') #Cargo las cuentas ya creadas
+        call_command('loaddata', 'data_usr.json', app_label='movimiento') #Cargo los usuarios ya creados
+        call_command('loaddata', 'data_cate.json', app_label='movimiento') #Cargo las categorias ya creadas
+        response = self.client.get('/controlgastos/movimientos/')
+        self.assertEqual(200, response.status_code)
+        
+    #Testeo la consulta de un movimiento especifico
+    def test_get_movimiento_bypk(self):
+        call_command('loaddata', 'data_acc.json', app_label='movimiento') #Cargo las cuentas ya creadas
+        call_command('loaddata', 'data_usr.json', app_label='movimiento') #Cargo los usuarios ya creados
+        call_command('loaddata', 'data_cate.json', app_label='movimiento') #Cargo las categorias ya creadas
+        call_command('loaddata', 'data_mov.json', app_label='movimiento') #Cargo los movimientos ya creados
+        response = self.client.get('/controlgastos/movimientos/1/')
+        self.assertEqual(200, response.status_code)
+       
+
+    '''Eliminacion de movimiento'''
+
+    #Testeo borrado de un movimiento
+    def test_delete_movimiento(self):
+        call_command('loaddata', 'data_acc.json', app_label='movimiento') #Cargo las cuentas ya creadas
+        call_command('loaddata', 'data_usr.json', app_label='movimiento') #Cargo los usuarios ya creados
+        call_command('loaddata', 'data_cate.json', app_label='movimiento') #Cargo las categorias ya creadas
+        call_command('loaddata', 'data_mov.json', app_label='movimiento') #Cargo los movimientos ya creados
+        response_delete = self.client.delete('/controlgastos/movimientos/1/', format='json')
+        self.assertEqual(200, response_delete.status_code)
+
+    #Testeo intento de borrado de un movimiento inexistente
+    def test_not_found_delete_movimiento_erroneo(self):
+        response_delete = self.client.delete('/controlgastos/movimientos/99/', format='json')
+        self.assertEqual(404, response_delete.status_code)
+
+
+    '''Modificacion de movimiento'''
+
+    #Testeo modificación de un movimiento
+    def test_put_movimiento(self):
+        call_command('loaddata', 'data_acc.json', app_label='movimiento') #Cargo las cuentas ya creadas
+        call_command('loaddata', 'data_usr.json', app_label='movimiento') #Cargo los usuarios ya creados
+        call_command('loaddata', 'data_cate.json', app_label='movimiento') #Cargo las categorias ya creadas
+        call_command('loaddata', 'data_mov.json', app_label='movimiento') #Cargo los movimientos ya creados
+        movimiento_data_modificada = {u'monto': u'20000', u'categoria': u'1', u'user': u'1',u'descripcion':u'alquileres'}
+        response_put = self.client.put('/controlgastos/movimientos/1/', movimiento_data_modificada, format='json')
+        self.assertEqual(200, response_put.status_code)
+
+    #Testeo modificación de un movimiento con envio incorrecto de parametros
+    def test_bad_request_put_usuario(self):
+        call_command('loaddata', 'data_acc.json', app_label='movimiento') #Cargo las cuentas ya creadas
+        call_command('loaddata', 'data_usr.json', app_label='movimiento') #Cargo los usuarios ya creados
+        call_command('loaddata', 'data_cate.json', app_label='movimiento') #Cargo las categorias ya creadas
+        call_command('loaddata', 'data_mov.json', app_label='movimiento') #Cargo los movimientos ya creados
+        movimiento_data_modificada = {u'monto': u'20000', u'categoria': u'ERROR', u'user': u'1',u'descripcion':u'alquileres'}
+        response_put = self.client.put('/controlgastos/movimientos/1/', movimiento_data_modificada, format='json')
+        self.assertEqual(400, response_put.status_code)
