7d16f1ab72924eb28cc2c03579b5edeb83f430f1
Victoria Sol Caparelli <vscaparelli@users.noreply.github.com>
2017-12-12 23:40:21 -0300

    Update tests.py

--- categoria/tests.py
+++ categoria/tests.py
@@ -1,43 +1,64 @@
 from rest_framework.test import APITestCase
 from django.core.management import call_command
 
-
+#TEST DEL RECURSO CATEGOGIA
 class CategoriaTestCase(APITestCase):
-    # Test module for Categoria model
-
+    
+    '''Alta de Categoria'''
+    
+    #Testeo la carga de una categoria
     def test_post_categoria(self):
         call_command('loaddata', 'data_acc.json', app_label='categoria')
         categoria_data = {u'nombre': u'testnombre', u'cuenta': u'1'}
         response = self.client.post('/controlgastos/categorias/', categoria_data, format='json')
         self.assertEqual(201, response.status_code)
-
-    def test_delete_categoria(self):
+        
+   #Testeo la carga de una categoria asociada a una cuenta inexistente
+   def test_bad_request_incorrect_type_categoria(self):
+        categoria_data_wrong = {u'nombre': u'testnombre', u'cuenta': u'100'}
+        response_post_wrong = self.client.post('/controlgastos/categorias/', categoria_data_wrong, format='json')
+        self.assertEqual(400, response_post_wrong.status_code)
+        
+    #Testeo la carga de una categoria con parametros incorrectos
+    def test_bad_request_field_required_categoria(self):
         call_command('loaddata', 'data_acc.json', app_label='categoria')
-        call_command('loaddata', 'data_cate.json', app_label='categoria')
-        responseDelete = self.client.delete('/controlgastos/categorias/1/', format='json')
-        self.assertEqual(200, responseDelete.status_code)
+        categoria_data_wrong = {u'cuenta': u'1'}
+        response_post_wrong = self.client.post('/controlgastos/categorias/', categoria_data_wrong, format='json')
+        self.assertEqual(400, response_post_wrong.status_code)
 
+   
+   '''Modificacion de Categoria''' 
+    
+    #Testeo modificacion de una categoria
     def test_put_categoria(self):
         call_command('loaddata', 'data_acc.json', app_label='categoria')
         call_command('loaddata', 'data_cate.json', app_label='categoria')
         categoria_data_modificada = {u'nombre': u'testnombremodificado', u'cuenta': u'1'}
         responsePut = self.client.put('/controlgastos/categorias/1/', categoria_data_modificada, format='json')
         self.assertEqual(200, responsePut.status_code)
+        
+   #Testeo modificación de categoria con envio incorrecto de parametros
+    def test_bad_request_put_usuario(self):
+        call_command('loaddata', 'data_acc.json', app_label='categoria') #Cargo las cuentas ya creadas
+        call_command('loaddata', 'data_usr.json', app_label='categoria') #Cargo un usuario ya creado
+        usuario_data_modificada = {u'password': u'testpassword2', u'user2': u'testuser', u'cuenta': u'ERROR',u'email': u'test@email.com'}
+        response_put = self.client.put('/controlgastos/usuarios/1/', usuario_data_modificada, format='json')
+        self.assertEqual(400, response_put.status_code) 
+        
+        
+   '''Eliminacion de Categoria'''
 
-    def test_bad_request_incorrect_type_categoria(self):
+    #Testeo de eliminacion de categoria
+    def test_delete_categoria(self):
         call_command('loaddata', 'data_acc.json', app_label='categoria')
-        categoria_data_wrong = {u'nombre': u'testnombre', u'cuenta': u'SoyUnError'}
-        response_post_wrong = self.client.post('/controlgastos/categorias/', categoria_data_wrong, format='json')
-        self.assertEqual(400, response_post_wrong.status_code)
+        call_command('loaddata', 'data_cate.json', app_label='categoria')
+        responseDelete = self.client.delete('/controlgastos/categorias/1/', format='json')
+        self.assertEqual(200, responseDelete.status_code)
+        
+    #Testeo intento de eliminacion de una categoria no creada
+    def test_not_found_delete_categoria_erroneo(self):
+        response_delete = self.client.delete('/controlgastos/categorias/10/', format='json')
+        self.assertEqual(404, response_delete.status_code)
+        
 
-    def test_bad_request_field_required_categoria(self):
-        call_command('loaddata', 'data_acc.json', app_label='usuario')
-        categoria_data_wrong = {u'cuenta': u'1'}
-        response_post_wrong = self.client.post('/controlgastos/categorias/', categoria_data_wrong, format='json')
-        self.assertEqual(400, response_post_wrong.status_code)
-
-    def test_bad_request_no_encuentra_recurso_categoria(self):
-        call_command('loaddata', 'data_acc.json', app_label='categoria')
-        categoria_data_wrong = {u'nombre': u'testnombre', u'cuenta': u'999'}
-        response_post_wrong = self.client.post('/controlgastos/categorias/', categoria_data_wrong, format='json')
-        self.assertEqual(400, response_post_wrong.status_code)
+ 
